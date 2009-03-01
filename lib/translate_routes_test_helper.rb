1883fa3d08cba24a3ddb4592847923ec694038a8
Raul Murciano <raul@murciano.net>
2009-03-01 04:28:51 +0100

    TestProcess#process_with_default_language signature updated following the rails2.3 TestProcess#process changes, thanks to Nico Ritsche

--- lib/translate_routes_test_helper.rb
+++ lib/translate_routes_test_helper.rb
@@ -6,10 +6,10 @@ require 'test_help'
 # Include default lang on your test requests (test requests doesn't support default_url_options):
 ActionController::TestProcess.class_eval do
   unless method_defined?(:process_without_default_language)
-    def process_with_default_language(action, parameters = nil, session = nil, flash = nil)
+    def process_with_default_language(action, parameters = nil, session = nil, flash = nil, http_method = 'GET')
       lang_pair = {:locale, I18n.default_locale.to_s}
       parameters = lang_pair.merge(parameters) rescue lang_pair
-      process_without_default_language(action, parameters, session, flash)
+      process_without_default_language(action, parameters, session, flash, http_method)
     end
 
     alias :process_without_default_language :process
