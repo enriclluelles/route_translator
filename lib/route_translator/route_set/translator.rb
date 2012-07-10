b55f8e37f3efd86c76cb73fd4ad13b93b661672b
Enric Lluelles <enric@lluell.es>
2012-07-10 20:50:48 +0200

    Remove regression when optional segments

--- lib/route_translator/route_set/translator.rb
+++ lib/route_translator/route_set/translator.rb
@@ -103,10 +103,11 @@ module RouteTranslator
 
       # Translates a path and adds the locale prefix.
       def translate_path(path, locale)
-        final_optional_segments = path.slice(/(\(.+\))$/)
+        final_optional_segments = path.slice!(/(\(.+\))$/)
         new_path = path.split("/").map{|seg| translate_path_segment(seg, locale)}.join('/')
         new_path = "/#{locale.downcase}#{new_path}" unless default_locale?(locale)
-        new_path.blank? ? "/" : "#{new_path}#{final_optional_segments}"
+        new_path = "/" if new_path.blank?
+        "#{new_path}#{final_optional_segments}"
       end
 
       # Tries to translate a single path segment. If the path segment
