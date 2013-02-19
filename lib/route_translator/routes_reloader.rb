12b3eeabe649937ef139c8ec510886a4ea962d9c
Enric Lluelles <enric@lluell.es>
2013-02-19 22:18:20 +0100

    Revert "Only one approach to reloading routes"
    This was breaking autoloading in versions of rails >= 3.1
    
    This reverts commit c2bfa52aad92590417a52679d96e078537b50f4b.

--- lib/route_translator/routes_reloader.rb
+++ lib/route_translator/routes_reloader.rb
@@ -2,6 +2,22 @@ require "rails"
 
 module Rails
   class Application
+    if defined?(RoutesReloader)
+      class RoutesReloader
+        alias :reload_without_translator! :reload!
+
+        def reload!
+          result = reload_without_translator!
+
+          route_sets.each do |routes|
+            routes.default_locale = I18n.default_locale
+            routes.translate_from_file
+          end
+
+          result
+        end
+      end
+    else
       alias :reload_routes_without_translator! :reload_routes!
       def reload_routes!
         result = reload_routes_without_translator!
@@ -11,5 +27,6 @@ module Rails
 
         result
       end
+    end
   end
 end
