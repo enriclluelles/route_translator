b4634c40c53362077c8eb36fb432120ce1fec272
Enric Lluelles <enric@lluell.es>
2013-05-01 23:21:06 +0200

    Adding the filter to action_controller by default

--- lib/route_translator/extensions/action_controller.rb
+++ lib/route_translator/extensions/action_controller.rb
@@ -2,6 +2,8 @@ require 'action_controller'
 
 module ActionController
   class Base
+    before_filter :set_locale_from_url
+
     def set_locale_from_url
       I18n.locale = params[RouteTranslator.locale_param_key]
     end
