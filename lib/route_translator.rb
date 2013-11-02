30206e849695bd385f6dded8769a836e128af068
Nikos Timiopulos <nikos@timi.cz>
2013-11-02 22:18:11 +0100

    Update route_translator.rb
    Add prefix option to configuration.

--- lib/route_translator.rb
+++ lib/route_translator.rb
@@ -10,13 +10,14 @@ module RouteTranslator
 
   TRANSLATABLE_SEGMENT = /^([-_a-zA-Z0-9]+)(\()?/.freeze
 
-  Configuration = Struct.new(:force_locale, :generate_unlocalized_routes, :translation_file, :locale_param_key)
+  Configuration = Struct.new(:force_locale, :generate_unlocalized_routes, :translation_file, :locale_param_key, :prefix)
 
   def self.config(&block)
     @config ||= Configuration.new
     @config.force_locale ||= false
     @config.generate_unlocalized_routes ||= false
     @config.locale_param_key ||= :locale
+    @config.prefix ||= true
     yield @config if block
     @config
   end
