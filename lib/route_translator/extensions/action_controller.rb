9aa339d0175326c70717b03267dc573cfaaacc05
Enric Lluelles <enric@lluell.es>
2014-04-18 16:30:08 +0200

    Remove useless code, fixes #52

--- lib/route_translator/extensions/action_controller.rb
+++ lib/route_translator/extensions/action_controller.rb
@@ -8,11 +8,4 @@ module ActionController
       I18n.with_locale params[RouteTranslator.locale_param_key], &block
     end
   end
-
-  class TestCase
-    include ActionController::UrlFor
-
-    delegate :env, :request, :to => :@controller
-    def _routes; @routes; end
-  end
 end
