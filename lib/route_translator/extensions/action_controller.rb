d40e9aa0d2903bf892611c4e9d979c179a623dc8
Gaël-Ian Havard <gael-ian@lunaweb.fr>
2014-07-04 12:15:58 +0200

    Ensure I18n.default_locale is always restored

--- lib/route_translator/extensions/action_controller.rb
+++ lib/route_translator/extensions/action_controller.rb
@@ -9,19 +9,25 @@ module ActionController
     end
 
     private
+
     def with_host_locale(&block)
-      original_default         = I18n.default_locale
-      original_locale          = I18n.locale
+      host_locale = RouteTranslator::Host.locale_from_host(request.host)
+      begin
+        if host_locale
+          original_default         = I18n.default_locale
+          original_locale          = I18n.locale
 
-      if host_locale =  RouteTranslator::Host.locale_from_host(request.host)
-        I18n.default_locale = host_locale
-        I18n.locale         = host_locale
+          I18n.default_locale = host_locale
+          I18n.locale         = host_locale
+        end
+
+        yield
+
+      ensure
+        I18n.default_locale = original_default if host_locale
+        I18n.locale         = original_locale  if host_locale
       end
-
-      yield
-
-      I18n.default_locale = original_default
-      I18n.locale         = original_locale
     end
+    
   end
 end
