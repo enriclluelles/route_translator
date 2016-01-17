10b5008ed23404c594fc23ee4f14e5bf39167676
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2016-01-17 22:52:46 +0100

    Fixed test for mounted engine.

--- test/integration/generated_path_test.rb
+++ test/integration/generated_path_test.rb
@@ -72,7 +72,7 @@ class GeneratedPathTest < ActionDispatch::IntegrationTest
     config_default_locale_settings 'es'
     config_generate_unlocalized_routes false
 
-    get '/es/engine'
+    get '/engine'
     assert_response :success
     assert_equal(response.body, '/es/blorgh/posts')
   end
