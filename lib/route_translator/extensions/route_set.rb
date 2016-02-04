04f1d6f831409c96b4011b754d226ecc19a7bd81
Geremia Taglialatela <tagliala.dev@gmail.com>
2016-02-04 16:13:03 +0100

    Add locale to route options constraints Fix rubocop config

--- lib/route_translator/extensions/route_set.rb
+++ lib/route_translator/extensions/route_set.rb
@@ -4,9 +4,9 @@ module ActionDispatch
   module Routing
     class RouteSet
       def add_localized_route(mapping, path_ast, name, anchor, scope, path, controller, default_action, to, via, formatted, options_constraints, options)
-        RouteTranslator::Translator.translations_for(self, path, name, options) do |translated_name, translated_path, translated_options|
+        RouteTranslator::Translator.translations_for(self, path, name, options_constraints, options) do |translated_name, translated_path, translated_options_constraints, translated_options|
           translated_path_ast = ::ActionDispatch::Journey::Parser.parse(translated_path)
-          translated_mapping  = ::ActionDispatch::Routing::Mapper::Mapping.build(scope, self, translated_path_ast, controller, default_action, to, via, formatted, options_constraints, anchor, translated_options)
+          translated_mapping  = ::ActionDispatch::Routing::Mapper::Mapping.build(scope, self, translated_path_ast, controller, default_action, to, via, formatted, translated_options_constraints, anchor, translated_options)
 
           add_route translated_mapping, translated_path_ast, translated_name, anchor
         end
