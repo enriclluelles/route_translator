6b26f8a678bc7f2ecad99a9f74ef449ab6190c74
Geremia Taglialatela <tagliala.dev@gmail.com>
2016-12-14 23:45:01 +0100

    Fix RuboCop offences

--- test/dummy/app/controllers/dummy_controller.rb
+++ test/dummy/app/controllers/dummy_controller.rb
@@ -3,18 +3,15 @@ class DummyController < ActionController::Base
     render text: I18n.locale
   end
 
-  def show
-  end
+  def show; end
 
   def native
     render text: show_path
   end
 
-  def optional
-  end
+  def optional; end
 
-  def prefixed_optional
-  end
+  def prefixed_optional; end
 
   def suffix
     render text: params[:id]
