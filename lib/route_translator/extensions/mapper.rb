47cd7286355b76c583e07b0a321141e96304ad69
Geremia Taglialatela <tagliala.dev@gmail.com>
2017-01-06 22:53:47 +0100

    Remove empty line for uniformity with Rails
    The corresponding `add_route` method in `actionpack` doesn’t have an
    empty line there

--- lib/route_translator/extensions/mapper.rb
+++ lib/route_translator/extensions/mapper.rb
@@ -37,7 +37,6 @@ module ActionDispatch
         ast = Journey::Parser.parse path
 
         mapping = Mapping.build(@scope, @set, ast, controller, default_action, to, via, formatted, options_constraints, anchor, options)
-
         @set.add_localized_route(mapping, ast, as, anchor, @scope, path, controller, default_action, to, via, formatted, options_constraints, options)
       end
       # rubocop:enable Lint/UnderscorePrefixedVariableName
