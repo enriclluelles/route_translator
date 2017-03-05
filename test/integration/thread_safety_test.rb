aa77f4ace99d8fbb5e5332f998f69b7a5e0b851f
Geremia Taglialatela <tagliala.dev@gmail.com>
2017-03-05 22:04:42 +0100

    Improve thread safety test

--- test/integration/thread_safety_test.rb
+++ test/integration/thread_safety_test.rb
@@ -3,8 +3,17 @@ require File.expand_path('../../test_helper', __FILE__)
 class ThreadSafetyTest < ActionDispatch::IntegrationTest
   include RouteTranslator::ConfigurationHelper
 
+  def setup
+    setup_config
+  end
+
+  def teardown
+    teardown_config
+  end
+
   def test_i18n_locale_thread_safe
     config_default_locale_settings 'en'
+
     get '/es/dummy'
     assert_equal 'es', @response.body
 
