cf0efdc2d33f6831b44c6c966a522d0417e3e747
Geremia Taglialatela <tagliala.dev@gmail.com>
2017-03-07 14:04:23 +0100

    Switch to render :plain
    `render :text` is deprecated in Rails 5.1 because it does not
    actually render a `text/plain` response.

--- test/dummy/app/controllers/dummy_controller.rb
+++ test/dummy/app/controllers/dummy_controller.rb
@@ -1,21 +1,21 @@
 class DummyController < ActionController::Base
   def dummy
-    render text: I18n.locale
+    render plain: I18n.locale
   end
 
   def native
-    render text: show_path
+    render plain: show_path
   end
 
   def suffix
-    render text: params[:id]
+    render plain: params[:id]
   end
 
   def engine_es
-    render text: blorgh_es.posts_path
+    render plain: blorgh_es.posts_path
   end
 
   def engine
-    render text: blorgh.posts_path
+    render plain: blorgh.posts_path
   end
 end
--- test/helpers/helper_test.rb
+++ test/helpers/helper_test.rb
@@ -2,7 +2,7 @@ require File.expand_path('../../test_helper', __FILE__)
 
 class HelperTestController < ActionController::Base
   def test
-    render text: nil
+    render plain: nil
   end
 end
 
