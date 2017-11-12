33b01e8836f50219b615d5859a30750573323991
Geremia Taglialatela <tagliala.dev@gmail.com>
2017-11-12 18:27:24 +0100

    Fix test for older Rails versions

--- test/integration/engine_test.rb
+++ test/integration/engine_test.rb
@@ -8,7 +8,15 @@ class RoutingTest < ActionDispatch::IntegrationTest
   def test_with_engine_inside_localized_block
     get '/engine_es'
     assert_response :success
-    assert_equal '/es/blorgh/posts', response.body
+
+    url =
+      if Rails.version < '5.1.3'
+        '/blorgh/posts'
+      else
+        '/es/blorgh/posts'
+      end
+
+    assert_equal url, response.body
   end
 
   def test_with_engine_outside_localized_block
