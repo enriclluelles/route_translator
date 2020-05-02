b9fa32f311b2c2e471945a0dcbd01edad12220f9
Geremia Taglialatela <tagliala.dev@gmail.com>
2020-05-02 18:06:48 +0200

    Refactor translate segment method

--- lib/route_translator/translator/path/segment.rb
+++ lib/route_translator/translator/path/segment.rb
@@ -31,11 +31,18 @@ module RouteTranslator
             end
           end
 
-          def translate_string(str, locale, scope)
+          def translate_segment_part(segment_part, locale, scope)
             sanitized_locale = RouteTranslator::LocaleSanitizer.sanitize(locale)
-            translated_resource = translate_resource(str, sanitized_locale, scope)
+            translated_resource = translate_resource(segment_part, sanitized_locale, scope)
 
             Addressable::URI.normalize_component translated_resource
+          end
+
+          def translate_string(segment, locale, scope)
+            appended_part = segment.slice!(/(\()$/)
+            segment_part = translatable_segment(segment)
+
+            (translate_segment_part(segment_part, locale, scope) || segment) + appended_part.to_s
           end
         end
 
@@ -49,19 +56,16 @@ module RouteTranslator
         # If there is no translation, the path segment is blank, begins with a
         # ":" (param key) or "*" (wildcard), the segment is returned untouched.
         def translate(segment, locale, scope)
-          return segment if segment.empty?
+          return segment if segment.empty? || segment.starts_with?('(', '*')
 
           if segment.starts_with?(':')
             named_param, hyphenized = segment.split('-', 2)
             return "#{named_param}-#{translate(hyphenized, locale, scope)}" if hyphenized
           end
 
-          return segment if segment.starts_with?('(', '*') || segment.include?(':')
+          return segment if segment.include?(':')
 
-          appended_part = segment.slice!(/(\()$/)
-          str = translatable_segment(segment)
-
-          (translate_string(str, locale, scope) || segment) + appended_part.to_s
+          translate_string(segment, locale, scope)
         end
       end
     end
