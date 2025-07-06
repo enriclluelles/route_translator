be880cf881cace3326b4a934251ba40aa394c7f7
Geremia Taglialatela <tagliala.dev@gmail.com>
2025-07-06 14:40:54 +0200

    Add Rails 8.1 edge support
    Fixes ArgumentError in add_route due to argument mismatch with
    Rails 8.1.0.edge
    
    Closes #323

--- lib/route_translator/extensions.rb
+++ lib/route_translator/extensions.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
-require_relative 'extensions/mapper'
-require_relative 'extensions/route_set'
+require 'action_dispatch'
+
+# TODO: Remove `else` branch when dropping Rails < 8.1 support
+if ActionDispatch::Routing::Mapper.instance_method(:add_route).arity == 12
+  require_relative 'extensions/mapper'
+  require_relative 'extensions/route_set'
+else
+  require_relative 'extensions/legacy/mapper'
+  require_relative 'extensions/legacy/route_set'
+end
+
 require_relative 'extensions/action_controller'
